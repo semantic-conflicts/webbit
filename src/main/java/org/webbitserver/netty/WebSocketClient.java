789a07999a9e5af4c3ee04c8c5d93c70060d2c2f
leusonmario <leusonmario@gmail.com>
2020-04-09 13:49:14 -0300

    'Trigger build #0d57793'

--- src/main/java/org/webbitserver/netty/WebSocketClient.java
+++ src/main/java/org/webbitserver/netty/WebSocketClient.java
@@ -1,5 +1,4 @@
 package org.webbitserver.netty;
-
 import org.jboss.netty.bootstrap.ClientBootstrap;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelFuture;
@@ -29,7 +28,6 @@ import org.webbitserver.handler.exceptions.PrintStackTraceExceptionHandler;
 import org.webbitserver.handler.exceptions.SilentExceptionHandler;
 import org.webbitserver.helpers.Base64;
 import org.webbitserver.helpers.SslFactory;
-
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
 import java.io.InputStream;
@@ -40,247 +38,209 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-
 import static org.jboss.netty.channel.Channels.pipeline;
-
 public class WebSocketClient implements WebSocket {
-    private static final String ACCEPT_GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-    private static final MessageDigest SHA_1;
-
-    static {
+  public static String ACCEPT_GUID="258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+  public static MessageDigest SHA_1;
+static {
+    try {
+      SHA_1=MessageDigest.getInstance("SHA1");
+    }
+ catch (    NoSuchAlgorithmException e) {
+      throw new InternalError("SHA-1 not supported on this platform");
+    }
+  }
+  public static long nextId=1;
+  public WebSocketHandler webSocketHandler;
+  public Executor executor;
+  public InetSocketAddress remoteAddress;
+  public HttpRequest request;
+  public boolean ssl;
+  public ClientBootstrap bootstrap;
+  public Channel channel;
+  public String base64Nonce;
+  public Thread.UncaughtExceptionHandler exceptionHandler;
+  public Thread.UncaughtExceptionHandler ioExceptionHandler;
+  public SslFactory sslFactory;
+  public WebSocketClient(  URI uri,  WebSocketHandler webSocketHandler){
+    this(uri,webSocketHandler,Executors.newSingleThreadExecutor());
+  }
+  public WebSocketClient(  URI uri,  WebSocketHandler webSocketHandler,  Executor executor){
+    this.webSocketHandler=webSocketHandler;
+    this.executor=executor;
+    String scheme=uri.getScheme() == null ? "ws" : uri.getScheme();
+    String host=uri.getHost() == null ? "localhost" : uri.getHost();
+    int port=uri.getPort();
+    ssl=scheme.equalsIgnoreCase("wss");
+    if (port == -1) {
+      if (scheme.equalsIgnoreCase("ws")) {
+        port=80;
+      }
+ else       if (ssl) {
+        port=443;
+      }
+    }
+    remoteAddress=new InetSocketAddress(host,port);
+    request=createNettyHttpRequest(uri.toASCIIString().replaceFirst("http","ws"),host);
+    uncaughtExceptionHandler(new PrintStackTraceExceptionHandler());
+    connectionExceptionHandler(new SilentExceptionHandler());
+  }
+  /** 
+ * What to do when an exception gets thrown in a handler. <p/> Defaults to using  {@link org.webbitserver.handler.exceptions.PrintStackTraceExceptionHandler}. It is suggested that apps supply their own implementation (e.g. to log somewhere).
+ */
+  WebSocket uncaughtExceptionHandler(  Thread.UncaughtExceptionHandler handler){
+    this.exceptionHandler=handler;
+    return this;
+  }
+  /** 
+ * What to do when an exception occurs when attempting to read/write data from/to the underlying connection. e.g. If an HTTP request disconnects before it was expected. <p/> Defaults to using  {@link org.webbitserver.handler.exceptions.SilentExceptionHandler}as this is a common thing to happen on a network, and most systems should not care.
+ */
+  WebSocket connectionExceptionHandler(  Thread.UncaughtExceptionHandler handler){
+    this.ioExceptionHandler=handler;
+    return this;
+  }
+  public WebSocketClient setupSsl(  InputStream keyStore,  String pass){
+    sslFactory=new SslFactory(keyStore,pass);
+    return this;
+  }
+  @Override public WebSocketClient start(){
+    final byte[] outboundMaskingKey=new byte[]{randomByte(),randomByte(),randomByte(),randomByte()};
+    bootstrap=new ClientBootstrap(new NioClientSocketChannelFactory(Executors.newCachedThreadPool(),Executors.newCachedThreadPool()));
+    bootstrap.setPipelineFactory(new ChannelPipelineFactory(){
+      public ChannelPipeline getPipeline() throws Exception {
+        ChannelPipeline pipeline=pipeline();
+        if (ssl) {
+          if (sslFactory == null) {
+            throw new WebbitException("You need to call setupSsl first");
+          }
+          SSLContext sslContext=sslFactory.getClientContext();
+          SSLEngine sslEngine=sslContext.createSSLEngine();
+          sslEngine.setUseClientMode(true);
+          pipeline.addLast("ssl",new SslHandler(sslEngine));
+        }
+        pipeline.addLast("decoder",new HttpResponseDecoder());
+        pipeline.addLast("encoder",new HttpRequestEncoder());
+        pipeline.addLast("inflater",new HttpContentDecompressor());
+        pipeline.addLast("handshakeHandler",new HandshakeChannelHandler(outboundMaskingKey));
+        return pipeline;
+      }
+    }
+);
+    ChannelFuture future=bootstrap.connect(remoteAddress);
+    channel=future.awaitUninterruptibly().getChannel();
+    if (!future.isSuccess()) {
+      close();
+    }
+ else {
+      channel.write(request).awaitUninterruptibly();
+    }
+    return this;
+  }
+  public HttpRequest createNettyHttpRequest(  String uri,  String host){
+    HttpRequest request=new DefaultHttpRequest(HttpVersion.HTTP_1_1,HttpMethod.GET,uri);
+    request.setHeader(HttpHeaders.Names.HOST,host);
+    request.setHeader(HttpHeaders.Names.CONNECTION,HttpHeaders.Values.KEEP_ALIVE);
+    request.setHeader(HttpHeaders.Names.ACCEPT_ENCODING,HttpHeaders.Values.GZIP);
+    request.setHeader(Hybi.SEC_WEBSOCKET_VERSION,13);
+    base64Nonce=base64Nonce();
+    request.setHeader(Hybi.SEC_WEBSOCKET_KEY,base64Nonce);
+    return request;
+  }
+  public String base64Nonce(){
+    byte[] nonce=new byte[16];
+    for (int i=0; i < 16; i++) {
+      nonce[i]=randomByte();
+    }
+    return Base64.encode(nonce);
+  }
+  public byte randomByte(){
+    return (byte)(Math.random() * 256);
+  }
+  @Override public WebSocket close(){
+    channel.getCloseFuture().awaitUninterruptibly();
+    bootstrap.releaseExternalResources();
+    executor.execute(new Runnable(){
+      @Override public void run(){
         try {
-            SHA_1 = MessageDigest.getInstance("SHA1");
-        } catch (NoSuchAlgorithmException e) {
-            throw new InternalError("SHA-1 not supported on this platform");
+          webSocketHandler.onClose(null);
         }
-    }
-
-    private static long nextId = 1;
-
-    private WebSocketHandler webSocketHandler;
-    private final Executor executor;
-    private final InetSocketAddress remoteAddress;
-    private final HttpRequest request;
-    private final boolean ssl;
-
-    private ClientBootstrap bootstrap;
-    private Channel channel;
-    private String base64Nonce;
-    private Thread.UncaughtExceptionHandler exceptionHandler;
-    private Thread.UncaughtExceptionHandler ioExceptionHandler;
-    private SslFactory sslFactory;
-
-    public WebSocketClient(URI uri, WebSocketHandler webSocketHandler) {
-        this(uri, webSocketHandler, Executors.newSingleThreadExecutor());
-    }
-
-    public WebSocketClient(URI uri, WebSocketHandler webSocketHandler, Executor executor) {
-        this.webSocketHandler = webSocketHandler;
-        this.executor = executor;
-
-        String scheme = uri.getScheme() == null ? "ws" : uri.getScheme();
-        String host = uri.getHost() == null ? "localhost" : uri.getHost();
-        int port = uri.getPort();
-        ssl = scheme.equalsIgnoreCase("wss");
-        if (port == -1) {
-            if (scheme.equalsIgnoreCase("ws")) {
-                port = 80;
-            } else if (ssl) {
-                port = 443;
-            }
+ catch (        Exception e) {
+          exceptionHandler.uncaughtException(Thread.currentThread(),WebbitException.fromException(e,channel));
         }
-        remoteAddress = new InetSocketAddress(host, port);
-        request = createNettyHttpRequest(uri.toASCIIString().replaceFirst("http", "ws"), host);
-
-        uncaughtExceptionHandler(new PrintStackTraceExceptionHandler());
-        connectionExceptionHandler(new SilentExceptionHandler());
+      }
     }
-
-    /**
-     * What to do when an exception gets thrown in a handler.
-     * <p/>
-     * Defaults to using {@link org.webbitserver.handler.exceptions.PrintStackTraceExceptionHandler}.
-     * It is suggested that apps supply their own implementation (e.g. to log somewhere).
-     */
-    WebSocket uncaughtExceptionHandler(Thread.UncaughtExceptionHandler handler) {
-        this.exceptionHandler = handler;
-        return this;
+);
+    return this;
+  }
+  @Override public WebSocketClient reconnectEvery(  long reconnectIntervalMillis){
+    webSocketHandler=new ReconnectingWebSocketHandler(webSocketHandler,WebSocketClient.this,reconnectIntervalMillis);
+    return this;
+  }
+public class HandshakeChannelHandler extends SimpleChannelUpstreamHandler {
+    public byte[] outboundMaskingKey;
+    public HandshakeChannelHandler(    byte[] outboundMaskingKey){
+      this.outboundMaskingKey=outboundMaskingKey;
     }
-
-    /**
-     * What to do when an exception occurs when attempting to read/write data
-     * from/to the underlying connection. e.g. If an HTTP request disconnects
-     * before it was expected.
-     * <p/>
-     * Defaults to using {@link org.webbitserver.handler.exceptions.SilentExceptionHandler}
-     * as this is a common thing to happen on a network, and most systems should not care.
-     */
-    WebSocket connectionExceptionHandler(Thread.UncaughtExceptionHandler handler) {
-        this.ioExceptionHandler = handler;
-        return this;
-    }
-
-    public WebSocketClient setupSsl(InputStream keyStore, String pass) {
-        sslFactory = new SslFactory(keyStore, pass);
-        return this;
-    }
-
-    @Override
-    public WebSocketClient start() {
-        final byte[] outboundMaskingKey = new byte[]{randomByte(), randomByte(), randomByte(), randomByte()};
-
-        bootstrap = new ClientBootstrap(new NioClientSocketChannelFactory(
-                Executors.newCachedThreadPool(),
-                Executors.newCachedThreadPool()));
-
-        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
-            public ChannelPipeline getPipeline() throws Exception {
-                ChannelPipeline pipeline = pipeline();
-                if (ssl) {
-                    if (sslFactory == null) {
-                        throw new WebbitException("You need to call setupSsl first");
-                    }
-                    SSLContext sslContext = sslFactory.getClientContext();
-                    SSLEngine sslEngine = sslContext.createSSLEngine();
-                    sslEngine.setUseClientMode(true);
-                    pipeline.addLast("ssl", new SslHandler(sslEngine));
-                }
-                pipeline.addLast("decoder", new HttpResponseDecoder());
-                pipeline.addLast("encoder", new HttpRequestEncoder());
-                pipeline.addLast("inflater", new HttpContentDecompressor());
-                pipeline.addLast("handshakeHandler", new HandshakeChannelHandler(outboundMaskingKey));
-                return pipeline;
-            }
-        });
-        ChannelFuture future = bootstrap.connect(remoteAddress);
-        channel = future.awaitUninterruptibly().getChannel();
-
-        if (!future.isSuccess()) {
-            close();
-        } else {
-            channel.write(request).awaitUninterruptibly();
+    @Override public void exceptionCaught(    ChannelHandlerContext ctx,    final ExceptionEvent e) throws Exception {
+      if (e.getCause() instanceof ClosedChannelException) {
+        e.getChannel().close();
+      }
+ else {
+        final Thread thread=Thread.currentThread();
+        executor.execute(new Runnable(){
+          @Override public void run(){
+            ioExceptionHandler.uncaughtException(thread,WebbitException.fromExceptionEvent(e));
+          }
         }
-        return this;
+);
+      }
     }
-
-    private HttpRequest createNettyHttpRequest(String uri, String host) {
-        HttpRequest request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, uri);
-        request.setHeader(HttpHeaders.Names.HOST, host);
-        request.setHeader(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
-        request.setHeader(HttpHeaders.Names.ACCEPT_ENCODING, HttpHeaders.Values.GZIP);
-        request.setHeader(Hybi.SEC_WEBSOCKET_VERSION, 13);
-
-        base64Nonce = base64Nonce();
-        request.setHeader(Hybi.SEC_WEBSOCKET_KEY, base64Nonce);
-        return request;
+    @Override public void messageReceived(    ChannelHandlerContext ctx,    MessageEvent e) throws Exception {
+      HttpResponse response=(HttpResponse)e.getMessage();
+      String webSocketAccept=response.getHeader(Hybi.SEC_WEBSOCKET_ACCEPT);
+      verifySecWebSocketAccept(webSocketAccept);
+      adjustPipelineToWebSocket(ctx,e,HybiWebSocketFrameDecoder.clientSide(outboundMaskingKey),new HybiWebSocketFrameEncoder());
     }
-
-    private String base64Nonce() {
-        byte[] nonce = new byte[16];
-        for (int i = 0; i < 16; i++) {
-            nonce[i] = randomByte();
+    public void verifySecWebSocketAccept(    String webSocketAccept){
+      if (webSocketAccept != null) {
+        SHA_1.reset();
+        SHA_1.update(base64Nonce.getBytes());
+        SHA_1.update(ACCEPT_GUID.getBytes());
+        String expectedKey=Base64.encode(SHA_1.digest());
+        if (!webSocketAccept.equals(expectedKey)) {
+          throw new WebbitException("Sec-WebSocket-Accept header from server didn't match expected value of " + expectedKey);
         }
-        return Base64.encode(nonce);
+      }
+ else {
+        throw new WebbitException("Expected Sec-WebSocket-Accept header from server");
+      }
     }
-
-    private byte randomByte() {
-        return (byte) (Math.random() * 256);
+    public void adjustPipelineToWebSocket(    ChannelHandlerContext ctx,    MessageEvent messageEvent,    ChannelHandler webSocketFrameDecoder,    ChannelHandler webSocketFrameEncoder){
+      NettyHttpRequest httpRequest=new NettyHttpRequest(messageEvent,request,nextId(),timestamp());
+      final NettyWebSocketConnection webSocketConnection=new NettyWebSocketConnection(executor,httpRequest,ctx,outboundMaskingKey);
+      webSocketConnection.setHybiWebSocketVersion(17);
+      ChannelHandler webSocketChannelHandler=new WebSocketConnectionHandler(webSocketConnection,exceptionHandler,ioExceptionHandler,webSocketHandler,executor);
+      ChannelPipeline p=ctx.getChannel().getPipeline();
+      p.remove("inflater");
+      p.replace("decoder","wsdecoder",webSocketFrameDecoder);
+      p.replace("encoder","wsencoder",webSocketFrameEncoder);
+      p.replace("handshakeHandler","wshandler",webSocketChannelHandler);
+      executor.execute(new CatchingRunnable(exceptionHandler){
+        @Override public void go() throws Exception {
+          webSocketHandler.onOpen(webSocketConnection);
+        }
+      }
+);
     }
-
-    @Override
-    public WebSocket close() {
-        channel.getCloseFuture().awaitUninterruptibly();
-        bootstrap.releaseExternalResources();
-        executor.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    webSocketHandler.onClose(null);
-                } catch (Exception e) {
-                    exceptionHandler.uncaughtException(Thread.currentThread(), WebbitException.fromException(e, channel));
-                }
-            }
-        });
-        return this;
+    public long timestamp(){
+      return System.currentTimeMillis();
     }
-
-    @Override
-    public WebSocketClient reconnectEvery(long reconnectIntervalMillis) {
-        webSocketHandler = new ReconnectingWebSocketHandler(webSocketHandler, WebSocketClient.this, reconnectIntervalMillis);
-        return this;
+    public HandshakeChannelHandler(){
     }
-
-    private class HandshakeChannelHandler extends SimpleChannelUpstreamHandler {
-        private final byte[] outboundMaskingKey;
-
-        public HandshakeChannelHandler(byte[] outboundMaskingKey) {
-            this.outboundMaskingKey = outboundMaskingKey;
-        }
-
-        @Override
-        public void exceptionCaught(ChannelHandlerContext ctx, final ExceptionEvent e) throws Exception {
-            if (e.getCause() instanceof ClosedChannelException) {
-                e.getChannel().close();
-            } else {
-                final Thread thread = Thread.currentThread();
-                executor.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        ioExceptionHandler.uncaughtException(thread, WebbitException.fromExceptionEvent(e));
-                    }
-                });
-            }
-        }
-
-        @Override
-        public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
-            HttpResponse response = (HttpResponse) e.getMessage();
-            String webSocketAccept = response.getHeader(Hybi.SEC_WEBSOCKET_ACCEPT);
-            verifySecWebSocketAccept(webSocketAccept);
-            adjustPipelineToWebSocket(ctx, e, HybiWebSocketFrameDecoder.clientSide(outboundMaskingKey), new HybiWebSocketFrameEncoder());
-        }
-
-        private void verifySecWebSocketAccept(String webSocketAccept) {
-            if (webSocketAccept != null) {
-                SHA_1.reset();
-                SHA_1.update(base64Nonce.getBytes());
-                SHA_1.update(ACCEPT_GUID.getBytes());
-                String expectedKey = Base64.encode(SHA_1.digest());
-                if (!webSocketAccept.equals(expectedKey)) {
-                    throw new WebbitException("Sec-WebSocket-Accept header from server didn't match expected value of " + expectedKey);
-                }
-            } else {
-                throw new WebbitException("Expected Sec-WebSocket-Accept header from server");
-            }
-        }
-
-        private void adjustPipelineToWebSocket(ChannelHandlerContext ctx, MessageEvent messageEvent, ChannelHandler webSocketFrameDecoder, ChannelHandler webSocketFrameEncoder) {
-            NettyHttpRequest httpRequest = new NettyHttpRequest(messageEvent, request, nextId(), timestamp());
-            final NettyWebSocketConnection webSocketConnection = new NettyWebSocketConnection(executor, httpRequest, ctx, outboundMaskingKey);
-            webSocketConnection.setHybiWebSocketVersion(17);
-
-            ChannelHandler webSocketChannelHandler = new WebSocketConnectionHandler(webSocketConnection, exceptionHandler, ioExceptionHandler, webSocketHandler, executor);
-
-            ChannelPipeline p = ctx.getChannel().getPipeline();
-            p.remove("inflater");
-            p.replace("decoder", "wsdecoder", webSocketFrameDecoder);
-            p.replace("encoder", "wsencoder", webSocketFrameEncoder);
-            p.replace("handshakeHandler", "wshandler", webSocketChannelHandler);
-
-            executor.execute(new CatchingRunnable(exceptionHandler) {
-                @Override
-                public void go() throws Exception {
-                    webSocketHandler.onOpen(webSocketConnection);
-                }
-            });
-        }
-
-        private long timestamp() {
-            return System.currentTimeMillis();
-        }
-    }
-
-    private static Object nextId() {
-        return nextId++;
-    }
+  }
+  public static Object nextId(){
+    return nextId++;
+  }
+  public WebSocketClient(){
+  }
 }
